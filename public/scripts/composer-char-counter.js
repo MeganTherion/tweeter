17d8fcfcfeed921de617218ae7aaa17f77fc8f05
MeganTherion <meganhutt@hotmail.com>
2022-05-18 03:45:02 +0000

    style tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,31 +1,24 @@
 $(document).ready(function() {
-console.log("ready bish");
+console.log("yallreadyforthis");
 
 //declare things you're gonna grab
 let counter = $('#counter');
 const tweetText = $('#tweet-text');
-let counterVal = counter.val();
 
 //declare callback function for handling typing event
 
 const handleTyping = function(event) {
   let numOfChars = (tweetText.val().length);
   let tweetBase = 140;
-  counter.html(function(i, val) {
-    
-      val = tweetBase-numOfChars;
-      if (val < 0) counter.css('color', 'red');
-      return val 
-    
+  counter.html(function(val) {
+   
+        val = tweetBase-numOfChars;
+        if (val < 0) counter.css('color', 'red');
+        else counter.css('color', '#4056A1');
+      return val
   })
-
 }
-
 
 //add event handler to own element
 tweetText.keydown(handleTyping);
 });
-
-// tweetText.addEventListener('keypress', function(event) {
-  
-// })
